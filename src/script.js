14385c76e52c00ef8e22933545f2d72a91851ec7
Надежда Иванова <forstudyni@yandex.ru>
2026-04-24 10:31:13 +0900

    слайдер вроде работает но стрелки говно, ещё ховеры сделала

--- src/script.js
+++ src/script.js
@@ -20,3 +20,29 @@ closeBtn.addEventListener('click', function(){
     }
     document.documentElement.style.overflow = '';
 })
+
+// ///////////////////
+const slider = document.getElementById('slider')
+const slides = document.querySelectorAll('.slide')
+let current = 0;
+
+function showSlide(ind) {
+    current = ind;
+    if (window.innerWidth < 768) {
+        slider.style.transform = `translateX(-${current * 90}vw)`;
+    }else if (window.innerWidth < 1024){
+        slider.style.transform = `translateX(-${current * 90}vw)`;
+    }else{
+        slider.style.transform = `translateX(-${current * 30}vw)`;
+    }
+    
+}
+function prevSlide() {
+    current = (current - 1 + slides.length) % slides.length
+    showSlide(current)
+}
+function nextSlide() {
+    current = (current + 1) % slides.length;
+    showSlide(current)
+}
+showSlide(0)
